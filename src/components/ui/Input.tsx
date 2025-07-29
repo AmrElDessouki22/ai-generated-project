22f34eeed1cf8f262188c3bcca0212f734ac0418
AmrElDessouki22 <47006473+AmrElDessouki22@users.noreply.github.com>
2025-07-29 16:24:47 +0300

    Add src/components/ui/Input.tsx

---
+++ src/components/ui/Input.tsx
@@ -0,0 +1,7 @@
+"use client";
+
+interface InputProps extends React.InputHTMLAttributes<HTMLInputElement> {}
+
+export default function Input(props: InputProps) {
+  return <input {...props} className="border p-2 rounded w-full" />;
+}
